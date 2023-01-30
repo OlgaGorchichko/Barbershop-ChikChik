5b54b187a00c9507ea3293454264d1b5b3909be4
Olga <olga@gorchichko.com>
2023-01-30 12:59:29 +0100

    submiting reservation on service

--- js/script.js
+++ js/script.js
@@ -226,7 +226,7 @@ const renderTime = (wrapper, data) => {
 
 const initReserve = () => {
     const reserveForm = document.querySelector('.reserve__form');
-    const { fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = reserveForm;
+    const { fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = reserveForm;
 
     addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
 
@@ -276,7 +276,7 @@ const initReserve = () => {
                 `${API_URL}api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}&day=${target.value}`);
             const data = await response.json();
 
-            fieldday.textContent = '';
+            fieldtime.textContent = '';
             renderTime(fieldtime, data);
             removePreload(fieldtime);
             removeDisabled([fieldtime]);
@@ -285,7 +285,35 @@ const initReserve = () => {
         if (target.name === 'time') {
             removeDisabled([btn]);
         }
-    })
+    });
+
+    reserveForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(reserveForm);
+        const json = JSON.stringify(Object.fromEntries(formData));
+
+        const response = await fetch(`${API_URL}api/order`, {
+            method: 'post',
+            body: json
+        });
+
+        const data = await response.json();
+
+        addDisabled([fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+
+        const p = document.createElement('p');
+        p.textContent = `
+            Спасибо за бронь №${data.id}!
+            Ждем вас ${new Intl.DateTimeFormat('ru-RU', {
+                month: 'long',
+                day: 'numeric'
+            }).format(new Date(`${data.month}/${data.day}`))}, 
+            время ${data.time}
+        `;
+
+        reserveForm.append(p);
+    });
 };
 
 const init = () => {
